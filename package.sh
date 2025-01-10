7b95259648829a41ce9b87a1fea93122d3398258
baltdev <heptor42+git@gmail.com>
2025-01-10 01:42:56 -0600

    Update packaging util

--- package.sh
+++ package.sh
@@ -1,7 +1,12 @@
-rm -rf pkg/*
+rm pkg.zip
+mkdir pkg
 cd Heresy
 dotnet build -c:Release -o:../bin
 cd ..
 cp bin pkg/bin -r
 cp manifest.json pkg
 cp icon.png pkg
+cd pkg
+zip ../pkg.zip ./* -r
+cd ..
+rm -rf pkg
